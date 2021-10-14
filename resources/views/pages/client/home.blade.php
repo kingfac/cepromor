1d702d488c4010c95a4067754afb0830420209ee
king <glodin.kiese@gmail.com>
2021-10-14 15:58:33 +0100

    header

--- resources/views/pages/client/home.blade.php
+++ resources/views/pages/client/home.blade.php
@@ -4,10 +4,10 @@
     {{-- <livewire:client.header /> --}}
     <livewire:client.nav />
     {{-- <livewire:client.home.nous /> --}}
-    <livewire:client.home.actualites />
+    {{-- <livewire:client.home.actualites />
     <livewire:client.home.projets />
     <livewire:client.home.volontaires />
     <livewire:client.home.temoignages />
-    <livewire:client.home.partenaires />
+    <livewire:client.home.partenaires /> --}}
     <livewire:client.footer />
 @endsection
