cd38e9508d472e4bd6eb5104e8937d4ecc277e81
king <glodin.kiese@gmail.com>
2021-10-14 18:31:29 +0100

    reparation

--- resources/views/pages/client/home.blade.php
+++ resources/views/pages/client/home.blade.php
@@ -1,13 +1,13 @@
 @extends('app')
 
 @section('content')
-    {{-- <livewire:client.header /> --}}
+    <livewire:client.header />
     <livewire:client.nav />
-    {{-- <livewire:client.home.nous /> --}}
-    {{-- <livewire:client.home.actualites />
+    <livewire:client.home.nous /> --}}
+    <livewire:client.home.actualites />
     <livewire:client.home.projets />
     <livewire:client.home.volontaires />
     <livewire:client.home.temoignages />
-    <livewire:client.home.partenaires /> --}}
+    <livewire:client.home.partenaires />
     <livewire:client.footer />
 @endsection
