ed90585dc6e30c8aa9c115a30cc6cc556f81d465
king <glodin.kiese@gmail.com>
2021-10-14 21:24:19 +0100

    reparation

--- resources/views/pages/client/home.blade.php
+++ resources/views/pages/client/home.blade.php
@@ -4,7 +4,7 @@
     <livewire:client.header />
     <livewire:client.nav />
     <livewire:client.home.nous />
-    <livewire:client.home.actualites />
+    {{-- <livewire:client.home.actualites /> --}}
     <livewire:client.home.projets />
     <livewire:client.home.volontaires />
     <livewire:client.home.temoignages />
