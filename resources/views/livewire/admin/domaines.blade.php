75d98093c4d6d95deec581e511fdc91d28771040
king <glodin.kiese@gmail.com>
2021-10-14 19:15:13 +0100

    reparation

--- resources/views/livewire/admin/domaines.blade.php
+++ resources/views/livewire/admin/domaines.blade.php
@@ -22,7 +22,6 @@
                                         <img src="{{ $photo->temporaryUrl() }}" alt="Pas d'image pour cette actualite" srcset="" class="flex-1">
                                     {{-- @endif --}}
                                     <h1 class="text-lg font-bold ">Previsualiser l'image</h1>
-                                    <p class="text-gray-400">{{$titre}}  {{$sous_titre}}</p>
                                 </a>      
                             @endif
                             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path></svg>
