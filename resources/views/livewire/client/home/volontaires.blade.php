749d9225104f1cef606da3473a74799f28ca51d6
king <glodin.kiese@gmail.com>
2021-10-14 21:57:17 +0100

    reparation

--- resources/views/livewire/client/home/volontaires.blade.php
+++ resources/views/livewire/client/home/volontaires.blade.php
@@ -8,18 +8,9 @@
         </div>
     </div>
     <div class="grid w-full grid-cols-1 gap-6 px-10 mt-10 md:grid-cols-2 xl:px-72 md:px-10 lg:px-20 animate__animated animate__bounceIn">
-        <div class="grid grid-cols-2 gap-4">
+        <div class="">
             <div class="">
                 <img src="{{asset('storage/volontaire/'.$volontaires[0]->id.'.png')}}" alt="" srcset="">
-            </div>
-            <div class="">
-                <img src="{{asset('storage/volontaire/'.$volontaires[1]->id.'.png')}}" alt="" srcset="">
-            </div>
-            <div class="">
-                <img src="{{asset('storage/volontaire/'.$volontaires[2]->id.'.png')}}" alt="" srcset="">
-            </div>
-            <div class="">
-                <img src="{{asset('storage/volontaire/'.$volontaires[3]->id.'.png')}}" alt="" srcset="">
             </div>
         </div>
         <div class="flex flex-col justify-center gap-4 p-5">
